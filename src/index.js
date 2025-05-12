57ca23f8466e032ec1898694c8951f18f1e86f55
Tomáš Mrázek <tomasmrazek92@gmail.com>
2025-05-12 15:50:12 +0200

    Modals added

--- src/index.js
+++ src/index.js
@@ -448,6 +448,63 @@ function animateTabs() {
     setInterval(window.refreshTabs, 2000);
   }
 }
+function initModal() {
+  function initModalBasic() {
+    const modalGroup = document.querySelector('[data-modal-group-status]');
+    const modals = document.querySelectorAll('[data-modal-name]');
+    const modalTargets = document.querySelectorAll('[data-modal-target]');
+
+    // Open modal
+    modalTargets.forEach((modalTarget) => {
+      modalTarget.addEventListener('click', function () {
+        const modalTargetName = this.getAttribute('data-modal-target');
+
+        // Close all modals
+        modalTargets.forEach((target) => target.setAttribute('data-modal-status', 'not-active'));
+        modals.forEach((modal) => modal.setAttribute('data-modal-status', 'not-active'));
+
+        // Activate clicked modal
+        document
+          .querySelector(`[data-modal-target="${modalTargetName}"]`)
+          .setAttribute('data-modal-status', 'active');
+        document
+          .querySelector(`[data-modal-name="${modalTargetName}"]`)
+          .setAttribute('data-modal-status', 'active');
+
+        // Set group to active
+        if (modalGroup) {
+          modalGroup.setAttribute('data-modal-group-status', 'active');
+        }
+      });
+    });
+
+    // Close modal
+    document.querySelectorAll('[data-modal-close]').forEach((closeBtn) => {
+      closeBtn.addEventListener('click', closeAllModals);
+    });
+
+    // Close modal on `Escape` key
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'Escape') {
+        closeAllModals();
+      }
+    });
+
+    // Function to close all modals
+    function closeAllModals() {
+      modalTargets.forEach((target) => target.setAttribute('data-modal-status', 'not-active'));
+
+      if (modalGroup) {
+        modalGroup.setAttribute('data-modal-group-status', 'not-active');
+      }
+    }
+  }
+
+  // Initialize Basic Modal
+  document.addEventListener('DOMContentLoaded', () => {
+    initModalBasic();
+  });
+}
 
 $(document).ready(function () {
   initNav();
